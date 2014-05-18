bf5c86b2f16d690698f1238909a1b94fdcc17b3e
Jeremy Stucki <jeremy@interactivethings.com>
2014-05-18 21:10:34 +0200

    Remove unnecessary check

--- src/get.js
+++ src/get.js
@@ -1,9 +1,8 @@
-var isUndefined = require('./util').isUndefined,
-    isNull = require('./util').isNull,
+var isNull = require('./util').isNull,
     isObject = require('./util').isObject;
 
 function get(obj, k, orValue) {
-  if (!isObject(obj) || isNull(obj) || !(k in obj)) return isUndefined(orValue) ? void 0 : orValue;
+  if (!isObject(obj) || isNull(obj) || !(k in obj)) return orValue;
   return obj[k];
 }
 
--- src/getIn.js
+++ src/getIn.js
@@ -1,12 +1,11 @@
-var isUndefined = require('./util').isUndefined,
-    isNull = require('./util').isNull,
+var isNull = require('./util').isNull,
     isObject = require('./util').isObject;
 
 // Get value from a nested structure or null.
 function getIn(obj, keys, orValue) {
   var k = keys[0],
       ks = keys.slice(1);
-  if (!isObject(obj) || isNull(obj) || !(k in obj)) return isUndefined(orValue) ? void 0 : orValue;
+  if (!isObject(obj) || isNull(obj) || !(k in obj)) return orValue;
   return ks.length ? getIn(obj[k], ks, orValue) : obj[k];
 }
 
