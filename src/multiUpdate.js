468f1604b4ff48cb35216168885a2978f23e268d
Jeremy Stucki <jeremy@interactivethings.com>
2014-06-19 11:04:04 +0200

    Remove unused argument

--- src/multiUpdate.js
+++ src/multiUpdate.js
@@ -2,7 +2,7 @@ var util = require('./util'),
     update = require('./update'),
     updateIn = require('./updateIn');
 
-function multiUpdate(obj, path, fn) {
+function multiUpdate(obj, path) {
   if (util.isArray(path)) return updateIn.apply(this, arguments);
   return update.apply(this, arguments);
 }
