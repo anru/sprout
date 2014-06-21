bd617ce03dca4810d79ce41af53d7597011293f9
Jeremy Stucki <jeremy@interactivethings.com>
2014-06-21 22:51:28 +0200

    Test for getting undefined/null values

--- test/get-test.js
+++ test/get-test.js
@@ -6,7 +6,9 @@ vows.describe('get()').addBatch({
   'Getting': {
     topic: function() {
       return {
-        foo: 1
+        foo: 1,
+        undef: undefined,
+        nil: null
       };
     },
     'an existing property returns the value': function(obj) {
@@ -22,6 +24,12 @@ vows.describe('get()').addBatch({
     'a non-existing property returns the default value if provided': function(obj) {
       assert.equal(get(obj, 'foo', 2), 1);
       assert.equal(get(obj, 'bar', 2), 2);
+    },
+    'an existing property with value undefined does not return the default value': function(obj) {
+      assert.isUndefined(get(obj, 'undef', 'not-found'));
+    },
+    'an existing property with value null does not return the default value': function(obj) {
+      assert.isNull(get(obj, 'nil', 'not-found'));
     },
   }
 }).export(module);
