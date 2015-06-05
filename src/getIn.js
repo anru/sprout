d5cab2ff1e7b9dae16643c8a63bb4047e52676f1
Bryan Helmig <bryan@zapier.com>
2015-06-05 00:23:05 -0700

    fix for getting undefined nested/nested values
    https://github.com/herrstucki/sprout/issues/5

--- src/getIn.js
+++ src/getIn.js
@@ -3,8 +3,9 @@ var get = require('./get');
 // Get value from a nested structure or null.
 function getIn(obj, keys, orValue) {
   var k = keys[0],
-      ks = keys.slice(1);
-  return ks.length ? getIn(obj[k], ks, orValue) : get(obj, k, orValue);
+      ks = keys.slice(1)
+      _obj = obj || {};
+  return ks.length ? getIn(_obj[k], ks, orValue) : get(_obj, k, orValue);
 }
 
 module.exports = getIn;
