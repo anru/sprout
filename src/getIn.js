b0323e0833169772483203b6119c3e8579033693
Bryan Helmig <bryan@zapier.com>
2015-06-05 00:27:49 -0700

    don't forget the comma...

--- src/getIn.js
+++ src/getIn.js
@@ -3,7 +3,7 @@ var get = require('./get');
 // Get value from a nested structure or null.
 function getIn(obj, keys, orValue) {
   var k = keys[0],
-      ks = keys.slice(1)
+      ks = keys.slice(1),
       _obj = obj || {};
   return ks.length ? getIn(_obj[k], ks, orValue) : get(_obj, k, orValue);
 }
