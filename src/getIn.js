d83eca8b1979eb01edce9ed490e238ee2e8c17fd
Jeremy Stucki <jeremy@interactivethings.com>
2015-07-10 08:42:37 +0200

    Optimize
    If we create empty objects and pass them in to `getIn`, we need to traverse all the keys. Checking for `k` in `obj` allows us to return early.

--- src/getIn.js
+++ src/getIn.js
@@ -3,9 +3,8 @@ var get = require('./get');
 // Get value from a nested structure or null.
 function getIn(obj, keys, orValue) {
   var k = keys[0],
-      ks = keys.slice(1),
-      _obj = obj || {};
-  return ks.length ? getIn(_obj[k], ks, orValue) : get(obj, k, orValue);
+      ks = keys.slice(1);
+  return get(obj, k) && ks.length ? getIn(obj[k], ks, orValue) : get(obj, k, orValue);
 }
 
 module.exports = getIn;
